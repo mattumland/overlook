3cd4eceb88b31a1cfc790f95fa2f2dd1551d3a3f
Matt Umland <mattumland@gmail.com>
2021-03-07 13:55:40 -0500

    Add iteration 1 without sorting

--- test/CurrentUser-test.js
+++ test/CurrentUser-test.js
@@ -9,17 +9,17 @@ describe('Current User', function() {
     });
 
     it('should be an instance of CurrentUser', function() {
-      const user = new CurrentUser();
+      const user = new CurrentUser({'id': 25, 'name': 'Bilboink Burgers'});
       expect(user).to.be.an.instanceof(CurrentUser);
     });
 
     it('should be able to have an id', function() {
-      const user = new CurrentUser(25);
+      const user = new CurrentUser({'id': 25, 'name': 'Bilboink Burgers'});
       expect(user.id).to.equal(25);
     });
 
     it('should be able to have a name', function() {
-      const user = new CurrentUser(25, 'Bilboink Burgers');
+      const user = new CurrentUser({'id': 25, 'name': 'Bilboink Burgers'});
       expect(user.id).to.equal(25);
       expect(user.name).to.equal('Bilboink Burgers');
     });
@@ -27,7 +27,7 @@ describe('Current User', function() {
 
   describe('Current User methods', function() {
     it('should be able to return the first name only', function() {
-      const user = new CurrentUser(25, 'Bilboink Burgers');
+      const user = new CurrentUser({'id': 25, 'name': 'Bilboink Burgers'});
       expect(user.getFirstName()).to.equal('Bilboink');
     })
 
